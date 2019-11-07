800d4d687f34d90574edd5de79a3ec47083d7503
Margie Ruffin <margieruffin@Magies-MacBook-Air.local>
2019-11-07 11:51:14 -0500

    updated database connection and authentication, users can create profiles

--- public/Authentication/authentication.js
+++ public/Authentication/authentication.js
@@ -1,6 +1,56 @@
-/**
-     * Handles the sign in button press.
-     */
+  /*
+       * initApp handles setting up UI event listeners and registering Firebase auth listeners:
+       *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
+       *    out, and that is where we update the UI.
+       */
+      function initApp() {
+        // Listening for auth state changes.
+        // [START authstatelistener]
+        firebase.auth().onAuthStateChanged(function(user) {
+          // [START_EXCLUDE silent]
+          document.getElementById('quickstart-verify-email').disabled = true;
+          // [END_EXCLUDE]
+          if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            var providerData = user.providerData;
+            // [START_EXCLUDE]
+            document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
+            document.getElementById('quickstart-sign-in').textContent = 'Sign out';
+            document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
+            if (!emailVerified) {
+              document.getElementById('quickstart-verify-email').disabled = false;
+            }
+            // [END_EXCLUDE]
+          } else {
+            // User is signed out.
+            // [START_EXCLUDE]
+            document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
+            document.getElementById('quickstart-sign-in').textContent = 'Sign in';
+            document.getElementById('quickstart-account-details').textContent = 'null';
+            // [END_EXCLUDE]
+          }
+          // [START_EXCLUDE silent]
+          document.getElementById('quickstart-sign-in').disabled = false;
+          // [END_EXCLUDE]
+        });
+        // [END authstatelistener]
+        //document.getElementById('quickstart-sign-in').addEventListener("DOMContentLoaded", 'click', toggleSignIn, false);
+        document.getElementById('quickstart-sign-up').addEventListener("DOMContentLoaded", 'click', handleSignUp, false);
+        document.getElementById('quickstart-verify-email').addEventListener("DOMContentLoaded", 'click', sendEmailVerification, false);
+        document.getElementById('quickstart-password-reset').addEventListener("DOMContentLoaded", 'click', sendPasswordReset, false);
+      }
+
+      window.onload = function() {
+        initApp();
+      };
+ /* Handles the sign in button press.
+  */
     function toggleSignIn() {
         if (firebase.auth().currentUser) {
           // [START signout]
@@ -67,7 +117,7 @@
           // [END_EXCLUDE]
         });
         // [END createwithemail]
-      }
+      } 
       /**
        * Sends an email verification to the user.
        */
@@ -81,10 +131,12 @@
         });
         // [END sendemailverification]
       }
+
+      
       function sendPasswordReset() {
         var email = document.getElementById('email').value;
         // [START sendpasswordemail]
-        firebase.auth().sendPasswordResetEmail(email).then(function() {
+        firebase. auth().sendPasswordResetEmail(email).then(function() {
           // Password Reset Email Sent!
           // [START_EXCLUDE]
           alert('Password Reset Email Sent!');
@@ -104,53 +156,6 @@
         });
         // [END sendpasswordemail];
       }
-      /**
-       * initApp handles setting up UI event listeners and registering Firebase auth listeners:
-       *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
-       *    out, and that is where we update the UI.
-       */
-      function initApp() {
-        // Listening for auth state changes.
-        // [START authstatelistener]
-        firebase.auth().onAuthStateChanged(function(user) {
-          // [START_EXCLUDE silent]
-          document.getElementById('quickstart-verify-email').disabled = true;
-          // [END_EXCLUDE]
-          if (user) {
-            // User is signed in.
-            var displayName = user.displayName;
-            var email = user.email;
-            var emailVerified = user.emailVerified;
-            var photoURL = user.photoURL;
-            var isAnonymous = user.isAnonymous;
-            var uid = user.uid;
-            var providerData = user.providerData;
-            // [START_EXCLUDE]
-            document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
-            document.getElementById('quickstart-sign-in').textContent = 'Sign out';
-            document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
-            if (!emailVerified) {
-              document.getElementById('quickstart-verify-email').disabled = false;
-            }
-            // [END_EXCLUDE]
-          } else {
-            // User is signed out.
-            // [START_EXCLUDE]
-            document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
-            document.getElementById('quickstart-sign-in').textContent = 'Sign in';
-            document.getElementById('quickstart-account-details').textContent = 'null';
-            // [END_EXCLUDE]
-          }
-          // [START_EXCLUDE silent]
-          document.getElementById('quickstart-sign-in').disabled = false;
-          // [END_EXCLUDE]
-        });
-        // [END authstatelistener]
-        document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
-        document.getElementById('quickstart-sign-up').addEventListener('click', handleSignUp, false);
-        document.getElementById('quickstart-verify-email').addEventListener('click', sendEmailVerification, false);
-        document.getElementById('quickstart-password-reset').addEventListener('click', sendPasswordReset, false);
-      }
-      window.onload = function() {
-        initApp();
-      };
+
+
+      
--- public/database.js
+++ public/database.js
@@ -1,28 +1,16 @@
 
-/*var firebaseConfig = {
-    apiKey: "AIzaSyBxsWriEmR_mauf_P9cycsofF2a1rU0i7g",
-    authDomain: "glassceilingswe.firebaseapp.com",
-    databaseURL: "https://glassceilingswe.firebaseio.com",
-    projectId: "glassceilingswe",
-    storageBucket: "glassceilingswe.appspot.com",
-    messagingSenderId: "318857051230",
-    appId: "1:318857051230:web:e8079bd12eb4d37a4dc8cf",
-    measurementId: "G-LEHC8NNE1X"
-  };
-
-  firebase.initializeApp(firebaseConfig);*/
 
   // Initialize Cloud Firestore through Firebase
-    firebase.initializeApp({
+  /*  firebase.initializeApp({
     apiKey: "AIzaSyBxsWriEmR_mauf_P9cycsofF2a1rU0i7g",
     authDomain: "glassceilingswe.firebaseapp.com",
     projectId: "glassceilingswe",
-  });
+  });*/
 
 
-  var database = firebase.firestore();
+  
 
-  /*<script type="text/javascript">
+  /*<script type="text/javascript"
     const docRef = database.doc("userProfiles/101");
     const outputHeader = document.querySelector("#text");
     const inputTextField = document.querySelector("#top-input");
@@ -43,79 +31,12 @@
       });
     })
 }
-    </script>
+    </script>*/
 
 
 
 
-/*const firebase = require("firebase");
-        // Required for side-effects
-        require("firebase/firestore");*/
 
 
 
-/*
 
-
-db.collection("CompanyProfile").doc("4001").set({
-    Benefits: " ",
-    Bias_Training: false,
-
-    Company_Name: " Google",
-    Fun_Facts: " We like to laugh"
-})
-.then(function() {
-    console.log("Document successfully written!");
-})
-.catch(function(error) {
-    console.error("Error writing document: ", error);
-});
-
-
-// Add a new document in collection "cities"
-db.collection("cities").doc("LA").set({
-    name: "Los Angeles",
-    state: "CA",
-    country: "USA"
-})
-.then(function() {
-    console.log("Document successfully written!");
-})
-.catch(function(error) {
-    console.error("Error writing document: ", error);
-});
-
-
-db.collection("Test Collection").add({
-    first: "Test 1",
-    last: "Test 2",
-    born: 1881
-})
-.then(function(docRef){
-    console.log("Document writeen with ID: ", docRef.id);
-})
-.catch(function(error){
-    console.error("Error adding document: ", error);
-});
-
-/* Add a second document with a generated ID.
-db.collection("users").add({
-    first: "Alan",
-    middle: "Mathison",
-    last: "Turing",
-    born: 1912
-})
-.then(function(docRef) {
-    console.log("Document written with ID: ", docRef.id);
-})
-.catch(function(error) {
-    console.error("Error adding document: ", error);
-});*/
-
-
-
-/*db.collection("users").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
-    });
-});*/
