585216e031b36b382dd678ba8f98d4bac5f6f5d7
Margie Ruffin <margieruffin@Magies-MacBook-Air.local>
2019-11-14 15:14:56 -0500

    created separate javascript files

---
+++ public/company/signup-login/comp-signup-auth.js
@@ -0,0 +1,57 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBxsWriEmR_mauf_P9cycsofF2a1rU0i7g",
+    authDomain: "glassceilingswe.firebaseapp.com",
+    databaseURL: "https://glassceilingswe.firebaseio.com",
+    projectId: "glassceilingswe",
+    storageBucket: "glassceilingswe.appspot.com",
+    messagingSenderId: "318857051230",
+    appId: "1:318857051230:web:e8079bd12eb4d37a4dc8cf",
+    measurementId: "G-LEHC8NNE1X"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+function handleSignUp() {
+    //save the name in the database maybe
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    //password length verification
+    if (email.length < 4) {
+      alert('Please enter an email address.');
+      return;
+    }
+    if (password.length < 4) {
+      alert('Please enter a password.');
+      return;
+    }
+    // Sign up with email and pass.
+    // [START createwithemail]
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+      
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // [START_EXCLUDE]
+      if (errorCode == 'auth/weak-password') {
+        alert('The password is too weak.');
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);
+      // [END_EXCLUDE]
+    });
+  // [END createwithemail]
+}
+
+  function sendEmailVerification() {
+    // [START sendemailverification]
+    firebase.auth().currentUser.sendEmailVerification().then(function() {
+      // Email Verification sent!
+      // [START_EXCLUDE]
+      alert('Email Verification Sent!');
+      top.location.href = "../company-profile/company.html";
+      // [END_EXCLUDE]
+    });
+    // [END sendemailverification]
+  }
